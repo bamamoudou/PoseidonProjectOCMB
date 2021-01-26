c1e5e648ee50f5dd04bf9eb02e0f7a0cf2e126a2
bamamoudou <mamoudouba8787@gmail.com>
2021-01-26 15:09:30 +0100

    Base code for project

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/BidListRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/BidListRepository.java
@@ -1,9 +1,7 @@
 package com.nnk.springboot.repositories;
 
-import com.nnk.springboot.domain.BidList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.nnk.springboot.domain.BidList;
 
-public interface BidListRepository extends JpaRepository<BidList, Integer> {
-
-}
+public interface BidListRepository extends JpaRepository<BidList, Integer> {}
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/TradeRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/TradeRepository.java
@@ -1,8 +1,7 @@
 package com.nnk.springboot.repositories;
 
-import com.nnk.springboot.domain.Trade;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.nnk.springboot.domain.Trade;
 
-public interface TradeRepository extends JpaRepository<Trade, Integer> {
-}
+public interface TradeRepository extends JpaRepository<Trade, Integer> {}
