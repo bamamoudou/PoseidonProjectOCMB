b9424aba65d36b8da00bde2669b760ae0fb8ac90
bamamoudou <mamoudouba8787@gmail.com>
2021-02-02 20:15:43 +0100

    Implement methods allow rating CRUD

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RatingController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RatingController.java
@@ -1,54 +1,91 @@
 package com.nnk.springboot.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nnk.springboot.domain.Rating;
+import com.nnk.springboot.servicesImpl.RatingServiceImpl;
 
 @RestController
 public class RatingController {
-	// TODO: Inject Rating service
+	/**
+	 * Inject Rating service
+	 */
+	@Autowired
+	private RatingServiceImpl ratingService;
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(RatingController.class);
 
 	@RequestMapping("/rating/list")
-	public String home(Model model) {
-		// TODO: find all Rating, add to model
-		return "rating/list";
+	public List<Rating> home() {
+		// find all Rating, add to model
+		return ratingService.findRatingByList();
 	}
 
-	@GetMapping("/rating/add")
-	public String addRatingForm(Rating rating) {
-		return "rating/add";
+	/**
+	 * Check data valid and save to db
+	 * 
+	 * @param rating
+	 * @return rating
+	 */
+	@PostMapping("/rating/add")
+	public ResponseEntity<Rating> addRating(@Valid @RequestBody Rating rating) {
+		return new ResponseEntity<Rating>(ratingService.saveRating(rating), HttpStatus.CREATED);
 	}
 
-	@PostMapping("/rating/validate")
-	public String validate(@Valid Rating rating, BindingResult result, Model model) {
-		// TODO: check data valid and save to db, after saving return Rating list
-		return "rating/add";
+	/**
+	 * Get Rating by Id
+	 * 
+	 * @param id
+	 * @return Rating
+	 */
+	@GetMapping("/rating/{id}")
+	public Rating getRatingById(@PathVariable Integer id) {
+		return ratingService.findRatingById(id);
 	}
 
-	@GetMapping("/rating/update/{id}")
-	public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-		// TODO: get Rating by Id and to model then show to the form
-		return "rating/update";
+	/**
+	 * Check required fields, if valid call service to update Rating
+	 * 
+	 * @param rating
+	 * @return Rating
+	 */
+	@PutMapping("/rating/update")
+	public Rating updateRating(@Valid @RequestBody Rating rating) {
+		return ratingService.updateRating(rating);
 	}
 
-	@PostMapping("/rating/update/{id}")
-	public String updateRating(@PathVariable("id") Integer id, @Valid Rating rating, BindingResult result, Model model) {
-		// TODO: check required fields, if valid call service to update Rating and
-		// return Rating list
-		return "redirect:/rating/list";
-	}
-
-	@GetMapping("/rating/delete/{id}")
-	public String deleteRating(@PathVariable("id") Integer id, Model model) {
-		// TODO: Find Rating by Id and delete the Rating, return to Rating list
-		return "redirect:/rating/list";
+	/**
+	 * Find Rating by Id and delete the Rating.
+	 * 
+	 * @param id
+	 */
+	@DeleteMapping("/rating/delete/{id}")
+	public void deleteRating(@PathVariable Integer id, HttpServletResponse response) {
+		Optional<Rating> bidOptional = Optional.ofNullable(ratingService.findRatingById(id));
+		if (bidOptional.isPresent()) {
+			LOGGER.info("delete, SUCCES");
+			response.setStatus(200);
+			ratingService.deleteRatingById(id);
+		} else {
+			LOGGER.error("Delete, ERROR");
+		}
 	}
 }
---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/controllers/RatingControllerTest.java
@@ -0,0 +1,87 @@
+package com.nnk.springboot.controllers;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nnk.springboot.domain.Rating;
+import com.nnk.springboot.servicesImpl.RatingServiceImpl;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(RatingController.class)
+public class RatingControllerTest {
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private WebApplicationContext webContext;
+
+	@MockBean
+	private RatingServiceImpl ratingService;
+
+	private Rating rating;
+	private List<Rating> ratingList;
+	private ObjectMapper mapper = new ObjectMapper();
+
+	@Before
+	public void setup() {
+		rating = new Rating(1, "Rating", "sandPRatin", "fitchRating", 10);
+		rating.setSandPRating("PRating");
+		mockMvc = MockMvcBuilders.webAppContextSetup(webContext).build();
+	}
+
+	@Test
+	public void findRatingByIdTest() throws Exception {
+		when(ratingService.findRatingById(1)).thenReturn(rating);
+		mockMvc.perform(get("/rating/ + 1")).andExpect(status().is2xxSuccessful());
+
+	}
+
+	@Test
+	public void findRatingByListTest() throws Exception {
+		when(ratingService.findRatingByList()).thenReturn(ratingList);
+		mockMvc.perform(
+				get("/rating/list").contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(rating)))
+				.andExpect(status().is2xxSuccessful());
+	}
+
+	@Test
+	public void addRatingTest() throws Exception {
+		when(ratingService.saveRating(rating)).thenReturn(rating);
+		mockMvc.perform(
+				post("/rating/add").contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(rating)))
+				.andExpect(status().isCreated());
+	}
+
+	@Test
+	public void updateRatingTest() throws JsonProcessingException, Exception {
+		when(ratingService.updateRating(rating)).thenReturn(rating);
+		mockMvc.perform(
+				put("/rating/update").contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(rating)))
+				.andExpect(status().is2xxSuccessful());
+
+	}
+
+	@Test
+	public void deleteRatingTest() throws Exception {
+		mockMvc.perform(delete("/rating/delete/" + 1)).andExpect(status().isOk());
+	}
+}
